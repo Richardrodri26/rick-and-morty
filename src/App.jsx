cb733e9fccbf94c78cb0c9ae257e076502e3194c
Richardrodri26 <richardmanuel811@gmail.com>
2023-01-01 12:27:26 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CharacterList from './components/CharacterList'
+
+function App() {
+
+  return (
+    <div className='bg-dark text-white'>
+      <h1 className='text-center display-1 py4'>Rick and Morty</h1>
+      <CharacterList/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useEffect, useState } from 'react'
+import Character from './Character'
+
+function NavPage(props) {
+  return (
+    <header className="d-flex justify-content-between align-items-center">
+      <button className=" btn-primary btn-sm"
+        onClick={()=> props.setPage(props.page > 1 ? props.page - 1 : error )}
+      >
+        Page {props.page - 1 }
+      </button>
+
+      <p>Page: {props.page}</p>
+
+      <button className=" btn-primary btn-sm"
+        onClick={()=> props.setPage(props.page + 1)}
+      >
+        Page {props.page + 1 }
+      </button>
+    </header>
+  )
+}
+
+function CharacterList() {
+    const [characters, setCharacter] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+
+    useEffect(()=>{
+      async function fetchData() {
+      const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
+      const data = await response.json();
+      setLoading(false);
+      setCharacter(data.results)
+      }
+  
+      fetchData();
+    }, [page])
+
+  return (
+    <div className="container">
+
+      <NavPage page= {page} setPage={setPage}/>  
+
+      {
+        loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div className="row">
+      {characters.map((character) => {
+        return (
+          <div className="col-md-4" key={character.id}>
+            <Character character= {character}/>
+          </div>
+        );
+      })}
+      </div>
+        )
+      }
+
+      <NavPage page= {page} setPage={setPage}/>  
+      
+    </div>
+  );
+}
+
+export default CharacterList;
